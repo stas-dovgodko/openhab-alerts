3e227a9681ed6684af45794b09fb5296ad9016ac
stas-dovgodko <standov@gmail.com>
2024-12-25 16:22:51 +0200

    1.0.0

--- index.js
+++ index.js
@@ -1,29 +1,56 @@
 const {rules, items, triggers, cache} = require('openhab');
-
-
+const historymanager = require('openhab-history');
 
 class Alerts {
-    constructor(group)
+    constructor(contact, history)
     {
-        this.group = group;
-        this.cache_key = 'alerts_' + group;
+        this.contact = contact;
+        this.history = history;
 
-        this.description = fires => {
-            let description = '';
-            for (const reason in fires ) {
-                const data = fires[reason];
-
-                if ('message' in data) {
-                    description += data.message + "\n";
-                } else if ('item' in data) {
-                    description += data.item.label + ' - ' + data.item.stateDescription +"\n";
+        try {
+            rules.when().item(this.history).receivedCommand().then(event => {
+                let payload = {};
+                try {
+                    if ((typeof items.getItem(this.history).state) === 'string') {
+                        payload = JSON.parse(items.getItem(this.history).state);
+    
+                        if ((typeof payload.state) === 'string') {
+                             items.getItem(payload.contact).postUpdate(payload.state);
+                            
+                        }
+                    }
+                } catch (e) {
+                    
                 }
+            }).build(this.history,'', [], this.history);
+        } catch (e) {
+            
+        }
+
+        this.payload = function(payload) {
+            //let before = items.getItem(this.history).history.latestState();
+            let before = items.getItem(this.history).state;
+    
+            let after = JSON.stringify(payload);
+
+            let description = '';
+            if ('message' in payload) {
+                description += payload.message;
+            } else if ('item' in payload) {
+                description += payload.item.label + ' - ' + payload.item.stateDescription;
             }
 
-            items.getItem(this.group).upsertMetadataValue('description', description);
-        };
+            if (after.localeCompare(before) !== 0) {
+                
+                items.getItem(this.history).sendCommand(after); // ignore undefined states
+                //items.getItem(this.history).history.persist();
+                items.getItem(this.contact).postUpdate(payload.state);
+                items.getItem(this.contact).replaceMetadata('description', description);
 
-        this.dumpe = reason => {
+            }
+        }
+
+        this.dumper = reason => {
             const item = items.getItem(reason);
             if (item) {
                 //let pattern = item.rawItem.getStateDescription().pattern ? item.rawItem.getStateDescription().pattern : '%s';
@@ -41,136 +68,151 @@ class Alerts {
                 return null;
             }
         };
-
-        this.pushEvent = e => {
-
-            let events = items.getItem(this.group).getMetadataValue('events');
-            events = events ? JSON.parse(events) : [];
-
-            events.push(e);
-            console.log('push: ' + events.length);
-            items.getItem(this.group).upsertMetadataValue('events', JSON.stringify(events));
-        };
-
-
-        this.popEvent = () => {
-            let events = items.getItem(this.group).getMetadataValue('events');
-            events = events ? JSON.parse(events) : [];
-
-            const e = events.pop();
-
-            console.log('pop: ' + events.length);
-
-            items.getItem(this.group).upsertMetadataValue('events', JSON.stringify(events));
-
-            return e;
-        };
     }
 
-    fire(reason, message) {
-        let fires = cache.get(this.cache_key, () => ({ }));
-        
-        const before = JSON.stringify(Object.keys(fires));
+    get fired() {
+        return items.getItem(this.contact).state == 'OPEN';
+    }
 
-        let data = {
-            reason: reason
+    history() {
+        return items.getItem(this.history).persistence;
+    }
+
+    fireItem(reason, debounce = 60) {
+        let t = time.ZonedDateTime.now().minusSeconds(debounce);
+        if (items.getItem(this.history).persistence.updatedSince(t)) { // bounce
+            return this;
+        }
+
+        let payload = {
+            state: 'OPEN'
         };
-        if (reason) {
-            const item = this.dumpe(reason);
-            if (item) {
-                data['item'] = item;
+        if (item) {
+            const d = this.dumper(item);
+            if (d) {
+                payload['item'] = d;
             }
         }
-        if (message) {
-            data['message'] = message
-        }
-        fires[((typeof reason) !== 'undefined') ? reason : 'default'] = data;
-        const after = JSON.stringify(Object.keys(fires));
 
-        this.description(fires);
-        
-        if (before != after) {
-            this.pushEvent(data);
-            cache.put(this.cache_key, fires);
-
-            items.getItem(this.group).postUpdate('OPEN');
-        }
+        this.payload(payload);
 
         return this;
     }
 
-
-
-    restore(reason) {
-        let fires = cache.get(this.cache_key, () => ({ }));
-
-        const before = JSON.stringify(Object.keys(fires));
-
-        if ((typeof reason) === 'string') {
-            delete fires[reason];
-        } else {
-            fires = {};
+    fire(message, debounce = 60) {
+        let t = time.ZonedDateTime.now().minusSeconds(debounce);
+        if (items.getItem(this.history).persistence.updatedSince(t)) { // bounce
+            return this;
         }
 
-        let data = {
-            reason: reason
+        let payload = {
+            state: 'OPEN'
         };
-        if (reason) {
-            const item = this.dumpe(reason);
-            if (item) {
-                data['item'] = item;
+        if (message) {
+            payload['message'] = message
+        }
+
+        this.payload(payload);
+
+        return this;
+    }
+
+    restore(message) {
+        let payload = {
+            state: 'CLOSED'
+        };
+        if (message) {
+            payload['message'] = message;
+        }
+
+        this.payload(payload);
+
+        return this;
+    }
+
+    restoreItem(item) {
+        let payload = {
+            state: 'CLOSED'
+        };
+        if (item) {
+            const d = this.dumper(item);
+            if (d) {
+                payload['item'] = d;
             }
         }
-        const after = JSON.stringify(Object.keys(fires));
 
-        this.description(fires);
-
-        if (before != after) {
-            
-            cache.put(this.cache_key, fires);
-
-            if (after == '[]') {
-                this.pushEvent(data);
-                items.getItem(this.group).postUpdate('CLOSED');
-            }
-        }
+        this.payload(payload);
 
         return this;
     }
 
     on(fireCallback, restoreCallback) {
+        if (this.contact !== null) {
+            
+            rules.JSRule({
+                name: `${this.contact}_events`,
+                //id: `${this.contact}_events`,
+                description: "[AUTO]",
+                triggers: [triggers.ItemStateUpdateTrigger(this.history)],
+                overwrite: true,
+                ruleGroup: "alerts",
+                execute: data => {
+                    if ((typeof items.getItem(this.history).state) === 'string') {
+                        const payload = JSON.parse(items.getItem(this.history).state);
 
-        items.getItem(this.group).upsertMetadataValue('events', JSON.stringify([]));
-        rules.JSRule({
-            name: this.group + ' Fire',
-            description: "[AUTO]",
-            triggers: [triggers.ItemStateUpdateTrigger(this.group, 'OPEN')],
-            execute: data => {
-                const e = this.popEvent();
-                if (e && (typeof fireCallback === 'function')) {
-                    fireCallback(e);
+                        try {
+                            
+
+                            if ((typeof payload.state) === 'string') {
+                                if ((payload.state === 'OPEN') && (typeof fireCallback === 'function')) {
+                                    fireCallback(this, payload);
+                                } else if ((payload.state === 'CLOSED') && (typeof restoreCallback === 'function')) {
+                                    restoreCallback(this, payload);
+                                }
+                            }
+                            
+                        } catch (e) {
+                            //console.log(e);
+                        }
+                    }
                 }
-
-                console.log('FIRE!');
-            }
-        });
-
-        rules.JSRule({
-            name: this.group + ' Restore',
-            description: "[AUTO]",
-            triggers: [triggers.ItemStateUpdateTrigger(this.group, 'CLOSED')],
-            execute: data => {
-                const e = this.popEvent();
-                if (e && (typeof restoreCallback === 'function')) {
-                    restoreCallback(e);
-                }
-            }
-        });
+            });
+        }
         
         return this;
     }
 }
 
-exports.type = (group) => {
-    return new Alerts(group);
+function contact(group) {
+    const history_item_name = `${group}_backlog`;
+
+    if (items.getItem(history_item_name, true) === null) {
+        items.addItem({
+            type: 'String',
+            name: history_item_name,
+            //label: `${this.contact.label} history`,
+            //category: 'light',
+            groups: [group, 'gPersist'],
+            //tags: this.contact.tags.concat(["History"]),
+            //metadata: {
+            /*expire: '10m,command=1',
+            stateDescription: {
+                config: {
+                pattern: '%d%%',
+                options: '1=Red, 2=Green, 3=Blue'
+                }
+            }*/
+            //}
+        });
+
+        
+    }
+
+    return new Alerts(group, history_item_name);
 };
+
+module.exports = new Proxy({contact}, {
+get: function (target, prop) {
+    return target[prop] || target.contact(prop);
+}
+});
